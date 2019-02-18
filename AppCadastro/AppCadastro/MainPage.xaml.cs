c78caaa358591adec071c7e883f4cb8e3961a2a6
CESAR AUGUSTO DE OLIVEIRA JUNIOR <cesar.aojunior@senacsp.edu.br>
2019-02-18 14:23:04 -0300

    Add project files.

---
+++ AppCadastro/AppCadastro/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCadastro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonEntrar_Clicked(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(entryLogin.Text) || string.IsNullOrEmpty(entrySenha.Text))
+            {
+                DisplayAlert("Ops...", "Não deixe os Campos em branco", "OK");
+            }
+            else
+            {
+                if(entryLogin.Text == "admin" && entrySenha.Text == "admin")
+                {
+                    
+                }
+                else
+                {
+                    DisplayAlert("Ops..", "Login e/ou senha invalidos", "OK");
+                }
+            }
+
+        }
+    }
+}
---
+++ AppCadastro/AppCadastro/Menu.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCadastro
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+            Detail = new NavigationPage(new MenuDetalhe());
+		}
+	}
+}
